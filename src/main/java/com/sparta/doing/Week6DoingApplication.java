d88e9aadd8c8e838b85b81141009e3a50a90c07f
Elrendar <guuto9@gmail.com>
2022-08-12 16:40:55 +0900

    Initial commit

---
+++ src/main/java/com/sparta/doing/Week6DoingApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.doing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week6DoingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week6DoingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/doing/Week6DoingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.doing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week6DoingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
