fc38a76a7a5c4995daca7532c1aefd481ecd16dd
kwanghunlee <lkhun9311@naver.com>
2022-08-14 21:25:52 +0900

    Add Week6DoingApplication에 에노테이션 추가, 서울 현재 시간으로 매칭해주는 메소드 생성.

--- src/main/java/com/sparta/doing/Week6DoingApplication.java
+++ src/main/java/com/sparta/doing/Week6DoingApplication.java
@@ -2,12 +2,24 @@ package com.sparta.doing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@ConfigurationPropertiesScan
+@EnableJpaAuditing
 @SpringBootApplication
 public class Week6DoingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Week6DoingApplication.class, args);
+    }
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
