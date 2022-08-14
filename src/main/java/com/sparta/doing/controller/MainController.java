e13d5700ffe22b46796fa042114e7b373208d348
kwanghunlee <lkhun9311@naver.com>
2022-08-14 21:23:16 +0900

    Add MainController 생성.

---
+++ src/main/java/com/sparta/doing/controller/MainController.java
@@ -0,0 +1,10 @@
+package com.sparta.doing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main() { return "forward:/boards"; }
+}
