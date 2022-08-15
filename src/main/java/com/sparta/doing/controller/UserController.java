e2a201451cdbce7c946cae84f5d480943121f66c
kwanghunlee <lkhun9311@naver.com>
2022-08-15 16:01:26 +0900

    Add UserController에서 JWT Token, Cookie, HttpHeaders 관련 코드를 추가하고 주석 처리함.

--- src/main/java/com/sparta/doing/controller/UserController.java
+++ src/main/java/com/sparta/doing/controller/UserController.java
@@ -13,6 +13,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -24,6 +27,20 @@ public class UserController {
     public ResponseEntity<UserResponseDto> signup(@RequestBody SignUpDto signUpDto) {
         return ResponseEntity.ok(userService.signup(signUpDto));
     }
+
+//    이광훈 팀원의 코드입니다. 참고하시면 좋겠습니다.
+//    @PostMapping("/signup")
+////    @ResponseStatus(value = HttpStatus.CREATED)
+//    public Response<UserAccountRequestDto> signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
+//        UserAccount userAccount = userService.signup(signupRequestDto);
+////        return Response.getData("회원가입 되었습니다.", UserAccountRequestDto.builder()
+////                .userId(userAccount.getUserId())
+////                .build());
+//        return Response.getData("회원가입 되었습니다.",
+//                UserAccountRequestDto.builder()
+//                        .userId(userAccount.getUserId())
+//                        .build());
+//    }
 
     // 회원 가입 페이지
     @GetMapping("/signup")
@@ -37,6 +54,39 @@ public class UserController {
         return ResponseEntity.ok(userService.login(loginDto));
     }
 
+//    이광훈 팀원의 코드입니다. 참고하시면 좋겠습니다.
+//    @PostMapping("/login")
+//    public ResponseEntity<Response<JwtTokenDto>> login(@RequestBody @Valid LoginRequestDto loginRequestDto,
+//                                                       HttpServletResponse httpServletResponse) {
+//        if(this.userService.login(loginRequestDto)){
+//            UsernamePasswordAuthenticationToken authenticationToken =
+//                    new UsernamePasswordAuthenticationToken(loginRequestDto.getUserId(), loginRequestDto.getPassword());
+//
+//            Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+//
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+//
+//            String jwtToken = jwtTokenProvider.createToken(authentication);
+//
+//            Cookie cookie = new Cookie("cookie", jwtToken);
+//            cookie.setHttpOnly(true);
+//            cookie.setMaxAge(300);
+//            httpServletResponse.addCookie(cookie);
+//
+//            HttpHeaders httpHeaders = new HttpHeaders();
+//
+//            Response<JwtTokenDto> response =
+//                    Response.getData("로그인 되었습니다.", JwtTokenDto.builder().jwtToken(jwtToken).build());
+//
+//            return new ResponseEntity<>(response, httpHeaders, HttpStatus.OK);
+//        } else {
+//            Response<JwtTokenDto> response = Response.getData("로그인 실패했습니다. 아이디 또는 비밀번호를 다시 입력해주세요."
+//                    , JwtTokenDto.builder().jwtToken("").build());
+//            HttpHeaders httpHeaders = new HttpHeaders();
+//            return new ResponseEntity<>(response, httpHeaders, HttpStatus.UNAUTHORIZED);
+//        }
+//    }
+
     // 회원 로그인 페이지
     @GetMapping("/loginview")
     public String login() {
@@ -48,6 +98,17 @@ public class UserController {
     public ResponseEntity<String> logout() {
         return ResponseEntity.ok(userService.logout());
     }
+
+//    이광훈 팀원의 코드입니다. 참고하시면 좋겠습니다.
+//    @GetMapping("/logout")
+//    public ResponseEntity<Response<String>> logout() {
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//
+//        Response<String> response = Response.getData("로그아웃 되었습니다.", null);
+//
+//        return new ResponseEntity<>(response, httpHeaders, HttpStatus.OK);
+//    }
+//}
 
     // 토큰 재발급
     @PostMapping("/renew")
