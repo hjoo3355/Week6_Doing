5dc5782d6ea0fb310cb80f6d34aa0a6afdd9cffb
hjoo3355 <96827462+hjoo3355@users.noreply.github.com>
2022-08-18 13:56:02 +0900

    mypage

--- src/main/java/com/sparta/doing/controller/MypageController.java
+++ src/main/java/com/sparta/doing/controller/MypageController.java
@@ -7,10 +7,12 @@ import com.sparta.doing.controller.request.TokenRequestDto;
 import com.sparta.doing.controller.response.TokenDto;
 import com.sparta.doing.controller.response.UserResponseDto;
 import com.sparta.doing.exception.RefreshTokenNotFoundException;
+import com.sparta.doing.service.BoardService;
 import com.sparta.doing.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -20,12 +22,27 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 @RequestMapping("/mypage")
 public class MypageController {
+    private final BoardService boardService;
+    private final BoardService customUserDetailsService;
     private final UserService userService;
 
-    @GetMapping("/api/user")
-    public String userInfo(@PathVariable Integer id, Model model) {
-        User userEntity = userService.회원정보(id);
-        model.addAttribute("user", userEntity);
-        return "user/updateForm";
-    }
-}
+    @GetMapping("/{id}")
+    public UserResponseDto userInfo(@PathVariable Long id) {
+        UserResponseDto responseDto = userService.getUserInfo(id);
+        return responseDto;
+      }
+//    @GetMapping("/myposts")
+//    public List<PostResponseDto> getUserPost (@AuthenticationPrincipal UserDetailsImpl userDetails) {
+//        return postService.getPosts(userDetails.getUser().getUsername());
+//    }
+//
+//    @GetMapping("/mycomments")
+//    public List<CommentResponseDto> getUserComments (@AuthenticationPrincipal UserDetailsImpl userDetails){
+//        return commentService.getComments(userDetails.getUser().getUsername());
+//    }
+//
+//    @GetMapping("/myreplies")
+//    public List<ReplyResponseDto> getUserReplies (@AuthenticationPrincipal UserDetailsImpl userDetails){
+//        return replyService.getReplies(userDetails.getUser().getUsername());
+//    }
+  }
